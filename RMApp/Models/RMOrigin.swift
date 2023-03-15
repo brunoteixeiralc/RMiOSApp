463cce443240d9010a2b2e0338cf12141bb50837
Bruno Lemgruber <brunoteixeiralc@gmail.com>
2023-03-15 15:29:56 -0300

    add models related do api

--- RMApp/Models/RMOrigin.swift
+++ RMApp/Models/RMOrigin.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+    
+}
--- RMApp/Models/RMSingleLocation.swift
+++ RMApp/Models/RMSingleLocation.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+    
+}
